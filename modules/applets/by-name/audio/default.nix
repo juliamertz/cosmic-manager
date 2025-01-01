c5968c5515de36ed8f47cb75f32c4e6f44f40bd3
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-01-01 04:01:06 -0300

    feat(applets): add Sound applet

---
+++ modules/applets/by-name/audio/default.nix
@@ -0,0 +1,26 @@
+{ lib, ... }:
+let
+  inherit (lib.cosmic.options) mkNullOrOption;
+in
+lib.cosmic.applets.mkCosmicApplet {
+  name = "audio";
+  originalName = "Sound";
+  identifier = "com.system76.CosmicAppletAudio";
+  configurationVersion = 1;
+
+  maintainers = [ lib.maintainers.HeitorAugustoLN ];
+
+  settingsOptions = {
+    show_media_controls_in_top_panel = mkNullOrOption {
+      type = lib.types.bool;
+      example = true;
+      description = ''
+        Whether to show media controls in the top panel.
+      '';
+    };
+  };
+
+  settingsExample = {
+    show_media_controls_in_top_panel = true;
+  };
+}
