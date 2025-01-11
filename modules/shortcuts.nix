51ac426cc53200041b2450a156e0e25ad389ef59
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-01-11 16:54:54 -0300

    refactor: improve RON type handling in shortcuts module
    - Use mkRonExpression for properly formatted examples
    - Add defaultNullOpts for shortcut description options
    - Wrap action type with maybeRonRaw for allowing to set raw RON values

--- modules/shortcuts.nix
+++ modules/shortcuts.nix
@@ -2,8 +2,15 @@
 let
   cfg = config.wayland.desktopManager.cosmic;
 
-  inherit (lib.cosmic.options) mkNullOrOption';
-  inherit (lib.cosmic.utils) capitalizeWord rustToNixType;
+  inherit (lib.cosmic)
+    capitalizeWord
+    cleanNullsExceptOptional
+    defaultNullOpts
+    isRonType
+    mkRonExpression
+    nestedLiteral
+    rustToNixType
+    ;
 in
 {
   options.wayland.desktopManager.cosmic.shortcuts =
@@ -14,18 +21,17 @@ in
         in
         lib.types.submodule {
           options = {
-            description = mkNullOrOption' {
-              type = with lib.types; ronOptionalOf str;
-              example = {
-                __type = "optional";
-                value = "Open Terminal";
-              };
-              description = ''
-                A description for the shortcut.
-                Used by COSMIC Settings to display the name of a custom shortcut.
-                This field is optional, and should only be used when defining custom shortcuts.
-              '';
-            };
+            description =
+              defaultNullOpts.mkRonOptionalOf lib.types.str
+                {
+                  __type = "optional";
+                  value = "Open Terminal";
+                }
+                ''
+                  A description for the shortcut.
+                  Used by COSMIC Settings to display the name of a custom shortcut.
+                  This field is optional, and should only be used when defining custom shortcuts.
+                '';
             key = lib.mkOption {
               type = lib.types.str;
               example = "Super+Q";
@@ -37,45 +43,47 @@ in
             action = lib.mkOption {
               type =
                 with lib.types;
-                oneOf (
-                  [
-                    (ronEnum (
-                      lib.pipe generatedActions [
-                        (builtins.getAttr "Actions")
-                        (builtins.filter (action: !(builtins.hasAttr "type" action)))
-                        (map (action: action.name))
-                      ]
-                    ))
-                  ]
-                  ++
-                    lib.mapAttrsToList
-                      (
-                        type: names:
-                        let
-                          actionDependencies = generatedActions.Dependencies;
-
-                          elemType =
-                            if builtins.hasAttr type actionDependencies then
-                              ronEnum (map (action: action.name) actionDependencies.${type})
-                            else
-                              rustToNixType type;
-                        in
-                        ronTupleEnumOf elemType names
-                      )
-                      (
+                maybeRonRaw (
+                  oneOf (
+                    [
+                      (ronEnum (
                         lib.pipe generatedActions [
                           (builtins.getAttr "Actions")
-                          (builtins.filter (action: builtins.hasAttr "type" action))
-                          (builtins.groupBy (action: action.type))
-                          (builtins.mapAttrs (_: actions: map (action: action.name) actions))
+                          (builtins.filter (action: !(builtins.hasAttr "type" action)))
+                          (map (action: action.name))
                         ]
-                      )
+                      ))
+                    ]
+                    ++
+                      lib.mapAttrsToList
+                        (
+                          type: names:
+                          let
+                            actionDependencies = generatedActions.Dependencies;
+
+                            elemType =
+                              if builtins.hasAttr type actionDependencies then
+                                ronEnum (map (action: action.name) actionDependencies.${type})
+                              else
+                                rustToNixType type;
+                          in
+                          ronTupleEnumOf elemType names
+                        )
+                        (
+                          lib.pipe generatedActions [
+                            (builtins.getAttr "Actions")
+                            (builtins.filter (action: builtins.hasAttr "type" action))
+                            (builtins.groupBy (action: action.type))
+                            (builtins.mapAttrs (_: actions: map (action: action.name) actions))
+                          ]
+                        )
+                  )
                 );
-              example = {
+              example = mkRonExpression 0 {
                 __type = "enum";
                 variant = "Spawn";
                 value = [ "firefox" ];
-              };
+              } null;
               description = ''
                 The action triggered by the shortcut.
                 Actions can include running a command, moving windows, system actions, and more.
@@ -84,40 +92,52 @@ in
           };
         };
     in
-    mkNullOrOption' {
+    lib.mkOption {
       type = lib.types.listOf shortcutSubmodule;
-      example = [
-        {
-          description = "Open Firefox";
-          key = "Super+B";
-          action = {
-            __type = "enum";
-            variant = "Spawn";
-            value = [ "firefox" ];
-          };
-        }
-        {
-          key = "Super+Q";
-          action = {
-            __type = "enum";
-            variant = "Close";
-          };
-        }
-        {
-          key = "Super+M";
-          action = {
-            __type = "enum";
-            variant = "Disable";
-          };
-        }
-        {
-          key = "XF86MonBrightnessDown";
-          action = {
-            __type = "enum";
-            variant = "BrightnessDown";
-          };
-        }
-      ];
+      default = [ ];
+      example =
+        let
+          shortcuts = [
+            {
+              description = "Open Firefox";
+              key = "Super+B";
+              action = {
+                __type = "enum";
+                variant = "Spawn";
+                value = [ "firefox" ];
+              };
+            }
+            {
+              key = "Super+Q";
+              action = {
+                __type = "enum";
+                variant = "Close";
+              };
+            }
+            {
+              key = "Super+M";
+              action = {
+                __type = "enum";
+                variant = "Disable";
+              };
+            }
+            {
+              key = "XF86MonBrightnessDown";
+              action = {
+                __type = "enum";
+                variant = "BrightnessDown";
+              };
+            }
+          ];
+        in
+        lib.pipe shortcuts [
+          (map (
+            shortcut:
+            builtins.mapAttrs (
+              _: value: if isRonType value then nestedLiteral (mkRonExpression 2 value null) else value
+            ) shortcut
+          ))
+        ];
       description = ''
         Defines a list of custom shortcuts for the COSMIC desktop environment.
         Each shortcut specifies a key combination, the action to be performed, and optionally a description for a custom shortcut.
@@ -129,7 +149,10 @@ in
       parseShortcuts =
         key:
         let
-          parts = builtins.filter (x: x != "") (lib.splitString "+" key);
+          parts = lib.pipe key [
+            (lib.splitString "+")
+            (builtins.filter (x: x != ""))
+          ];
 
           validModifiers = [
             "Alt"
@@ -149,31 +172,40 @@ in
               throw "Key cannot be a modifier"
             else
               last;
-          modifiers = map (
-            modifier:
-            if builtins.elem modifier validModifiers then
-              {
-                __type = "enum";
-                variant = modifier;
-              }
-            else
-              throw "Invalid modifier: ${modifier}. Valid modifiers are: ${builtins.concatStringsSep ", " validModifiers}"
-          ) (lib.unique (lib.init parts));
+          modifiers =
+            map
+              (
+                modifier:
+                if builtins.elem modifier validModifiers then
+                  {
+                    __type = "enum";
+                    variant = modifier;
+                  }
+                else
+                  throw "Invalid modifier: ${modifier}. Valid modifiers are: ${builtins.concatStringsSep ", " validModifiers}"
+              )
+              (
+                lib.pipe parts [
+                  lib.init
+                  lib.unique
+                ]
+              );
         };
     in
-    lib.mkIf (cfg.enable && cfg.shortcuts != null) {
+    lib.mkIf (cfg.shortcuts != [ ]) {
       wayland.desktopManager.cosmic.configFile."com.system76.CosmicSettings.Shortcuts" = {
         entries.custom = {
           __type = "map";
-          value = map (shortcut: {
-            key = lib.cosmic.utils.cleanNullsExceptOptional (
-              parseShortcuts shortcut.key
-              // {
-                inherit (shortcut) description;
-              }
-            );
-            value = shortcut.action;
-          }) cfg.shortcuts;
+          value = lib.pipe cfg.shortcuts [
+            (map (shortcut: {
+              key = lib.pipe shortcut.key [
+                parseShortcuts
+                (parsed: parsed // { inherit (shortcut) description; })
+                cleanNullsExceptOptional
+              ];
+              value = shortcut.action;
+            }))
+          ];
         };
         version = 1;
       };
