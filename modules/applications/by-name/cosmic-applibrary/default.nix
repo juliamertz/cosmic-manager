5eea3784ebb62fb7b3163a7ae4beb9dae3fae3b4
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-01-21 17:01:07 -0300

    feat(modules): add cosmic-applibrary module
    Add configuration module for COSMIC Application Library with support for:
    - Custom application groups configuration
    - Group filtering by categories or specific app IDs
    - Include/exclude rules for applications
    - Group icon customization
    
    Includes example configuration for office and games categories.

---
+++ modules/applications/by-name/cosmic-applibrary/default.nix
@@ -0,0 +1,165 @@
+{ lib, ... }:
+lib.cosmic.applications.mkCosmicApplication {
+  name = "cosmic-applibrary";
+  originalName = "COSMIC Application Library";
+  identifier = "com.system76.CosmicAppLibrary";
+  configurationVersion = 1;
+
+  maintainers = [ lib.maintainers.HeitorAugustoLN ];
+
+  settingsOptions =
+    let
+      inherit (lib.cosmic) defaultNullOpts;
+    in
+    {
+      groups =
+        let
+          groupsSubmodule = lib.types.submodule {
+            freeformType = with lib.types; attrsOf anything;
+            options = {
+              filter = lib.mkOption {
+                type =
+                  let
+                    categorySubmodule = lib.types.submodule {
+                      freeformType = with lib.types; attrsOf anything;
+                      options = {
+                        categories = lib.mkOption {
+                          type = with lib.types; listOf str;
+                          example = [ "Office" ];
+                          description = ''
+                            The categories of the group.
+                          '';
+                        };
+
+                        exclude = lib.mkOption {
+                          type = with lib.types; listOf str;
+                          example = [ "com.system76.CosmicStore" ];
+                          description = ''
+                            The applications to exclude from the group.
+                          '';
+                        };
+
+                        include = lib.mkOption {
+                          type = with lib.types; listOf str;
+                          example = [ "com.system76.CosmicStore" ];
+                          description = ''
+                            The applications to include in the group.
+                          '';
+                        };
+                      };
+                    };
+                  in
+                  with lib.types;
+                  oneOf [
+                    (ronEnum [ "None" ])
+                    (ronTupleEnumOf (listOf str) [ "AppIds" ] 1)
+                    (ronNamedStructOf categorySubmodule)
+                  ];
+                example = {
+                  __type = "namedStruct";
+                  name = "Categories";
+                  value = {
+                    categories = [ "Office" ];
+                    exclude = [ ];
+                    include = [
+                      "org.gnome.Totem"
+                      "org.gnome.eog"
+                      "simple-scan"
+                      "thunderbird"
+                    ];
+                  };
+                };
+                description = ''
+                  The filter of the group.
+                '';
+              };
+
+              icon = lib.mkOption {
+                type = lib.types.str;
+                example = "folder-symbolic";
+                description = ''
+                  The icon of the group.
+                '';
+              };
+
+              name = lib.mkOption {
+                type = lib.types.str;
+                example = "cosmic-office";
+                description = ''
+                  The name of the group.
+                '';
+              };
+            };
+          };
+        in
+        defaultNullOpts.mkNullable (lib.types.listOf groupsSubmodule)
+          [
+            {
+              name = "cosmic-office";
+              icon = "folder-symbolic";
+              filter = {
+                __type = "namedStruct";
+                name = "Categories";
+                value = {
+                  categories = [ "Office" ];
+                  exclude = [ ];
+                  include = [
+                    "org.gnome.Totem"
+                    "org.gnome.eog"
+                    "simple-scan"
+                    "thunderbird"
+                  ];
+                };
+              };
+            }
+            {
+              name = "Games";
+              icon = "folder-symbolic";
+              filter = {
+                __type = "enum";
+                variant = "AppIds";
+                value = [
+                  "Counter-Strike 2"
+                ];
+              };
+            }
+          ]
+          ''
+            The groups of applications to display.
+          '';
+    };
+
+  settingsExample = {
+    groups = [
+      {
+        name = "cosmic-office";
+        icon = "folder-symbolic";
+        filter = {
+          __type = "namedStruct";
+          name = "Categories";
+          value = {
+            categories = [ "Office" ];
+            exclude = [ ];
+            include = [
+              "org.gnome.Totem"
+              "org.gnome.eog"
+              "simple-scan"
+              "thunderbird"
+            ];
+          };
+        };
+      }
+      {
+        name = "Games";
+        icon = "folder-symbolic";
+        filter = {
+          __type = "enum";
+          variant = "AppIds";
+          value = [
+            "Counter-Strike 2"
+          ];
+        };
+      }
+    ];
+  };
+}
