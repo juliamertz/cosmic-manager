446afcd45e82c1bc268cf8488a89083fa2a5e079
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-01-30 01:49:09 -0300

    refactor(generators): replace throw statements with assertions
    - Replace explicit throw statements with lib.assertMsg for better error handling
    - Move trimFloatString function definition inside float case for better scoping
    - Improve code organization and readability in toRON generator
    - Use assertion-based validation for type checking and value requirements

--- lib/generators.nix
+++ lib/generators.nix
@@ -9,19 +9,22 @@
           nextIndent = startIndent + 1;
 
           indent = level: lib.strings.replicate level "    ";
-          trimFloatString =
-            float:
-            let
-              string = lib.strings.floatToString float;
-            in
-            if lib.strings.hasInfix "." string then
-              builtins.head (builtins.match "([0-9]+[.][0-9]*[1-9]|[0-9]+[.]0)0*" string)
-            else
-              string;
         in
         {
           bool = lib.boolToString value;
-          float = trimFloatString value;
+          float =
+            let
+              trimFloatString =
+                float:
+                let
+                  string = lib.strings.floatToString float;
+                in
+                if lib.strings.hasInfix "." string then
+                  builtins.head (builtins.match "([0-9]+[.][0-9]*[1-9]|[0-9]+[.]0)0*" string)
+                else
+                  string;
+            in
+            trimFloatString value;
           int = toString value;
           lambda = throw "Functions are not supported in RON";
           list =
@@ -47,135 +50,127 @@
           set =
             if value ? __type then
               if value.__type == "raw" then
-                if value ? value then
-                  if builtins.isString value.value then
-                    value.value
-                  else
-                    throw "lib.cosmic.generators.toRON: raw type value must be a string."
-                else
-                  throw "lib.cosmic.generators.toRON: raw type must have a value."
+                assert lib.assertMsg (value ? value) "lib.cosmic.generators.toRON: raw type must have a value.";
+                assert lib.assertMsg (builtins.isString value.value)
+                  "lib.cosmic.generators.toRON: raw type value must be a string.";
+
+                value.value
               else if value.__type == "optional" then
-                if value ? value then
-                  if value.value == null then "None" else "Some(${toRON' startIndent value.value})"
-                else
-                  throw "lib.cosmic.generators.toRON: optional type must have a value."
+                assert lib.assertMsg (
+                  value ? value
+                ) "lib.cosmic.generators.toRON: optional type must have a value.";
+
+                if value.value == null then "None" else "Some(${toRON' startIndent value.value})"
               else if value.__type == "char" then
-                if value ? value then
-                  if builtins.isString value.value then
-                    if builtins.stringLength value.value == 1 then
-                      "'${value.value}'"
-                    else
-                      throw "lib.cosmic.generators.toRON: char type must be a single character."
-                  else
-                    throw "lib.cosmic.generators.toRON: char type must be a string value."
-                else
-                  throw "lib.cosmic.generators.toRON: char type must have a value."
+                assert lib.assertMsg (value ? value) "lib.cosmic.generators.toRON: char type must have a value.";
+                assert lib.assertMsg (builtins.isString value.value)
+                  "lib.cosmic.generators.toRON: char type value must be a string.";
+                assert lib.assertMsg (
+                  builtins.stringLength value.value == 1
+                ) "lib.cosmic.generators.toRON: char type value must be a single character string.";
+
+                "'${value.value}'"
               else if value.__type == "enum" then
-                if value ? variant then
-                  if value ? value then
-                    if builtins.isList value.value then
-                      let
-                        count = builtins.length value.value;
-                      in
-                      if count == 0 then
-                        "${value.variant}()"
-                      else
-                        "${value.variant}(\n${
-                          lib.concatImapStringsSep "\n" (
-                            index: element:
-                            "${indent nextIndent}${toRON' nextIndent element}${lib.optionalString (index != count) ","}"
-                          ) value.value
-                        },\n${indent startIndent})"
-                    else
-                      throw "lib.cosmic.generators.toRON: enum type must have a list of values."
-                  else if builtins.isString value.variant then
-                    value.variant
+                assert lib.assertMsg (
+                  value ? variant
+                ) "lib.cosmic.generators.toRON: enum type must have a variant.";
+                assert lib.assertMsg (builtins.isString value.variant)
+                  "lib.cosmic.generators.toRON: enum type variant must be a string value.";
+
+                if value ? value then
+                  assert lib.assertMsg (builtins.isList value.value)
+                    "lib.cosmic.generators.toRON: enum type must have a list of values.";
+
+                  let
+                    count = builtins.length value.value;
+                  in
+                  if count == 0 then
+                    "${value.variant}()"
                   else
-                    throw "lib.cosmic.generators.toRON: enum type variant must be a string value."
+                    "${value.variant}(\n${
+                      lib.concatImapStringsSep "\n" (
+                        index: element:
+                        "${indent nextIndent}${toRON' nextIndent element}${lib.optionalString (index != count) ","}"
+                      ) value.value
+                    },\n${indent startIndent})"
                 else
-                  throw "lib.cosmic.generators.toRON: enum type must have a variant."
+                  value.variant
               else if value.__type == "map" then
-                if value ? value then
-                  if builtins.isList value.value then
-                    if builtins.all (x: builtins.isAttrs x) value.value then
-                      let
-                        count = builtins.length value.value;
-                      in
-                      if count == 0 then
-                        "{}"
-                      else
-                        "{\n${
-                          lib.concatImapStringsSep "\n" (
-                            index: entry:
-                            let
-                              keys = builtins.attrNames entry;
-                            in
-                            if
-                              [
-                                "key"
-                                "value"
-                              ] == keys
-                            then
-                              "${indent nextIndent}${toRON' nextIndent entry.key}: ${toRON' nextIndent entry.value}${
-                                lib.optionalString (index != count) ","
-                              }"
-                            else
-                              throw "lib.cosmic.generators.toRON: map type entry must have only 'key' and 'value' attributes."
-                          ) value.value
-                        },\n${indent startIndent}}"
-                    else
-                      throw "lib.cosmic.generators.toRON: map type value must be a list of attribute sets."
-                  else
-                    throw "lib.cosmic.generators.toRON: map type value must be a list."
+                assert lib.assertMsg (value ? value) "lib.cosmic.generators.toRON: map type must have a value.";
+                assert lib.assertMsg (builtins.isList value.value)
+                  "lib.cosmic.generators.toRON: map type value must be a list.";
+                assert lib.assertMsg (builtins.all (
+                  element: builtins.isAttrs element
+                ) value.value) "lib.cosmic.generators.toRON: map type value must be a list of attribute sets.";
+
+                let
+                  count = builtins.length value.value;
+                in
+                if count == 0 then
+                  "{}"
                 else
-                  throw "lib.cosmic.generators.toRON: map type must have a value."
-              else if value.__type == "tuple" then
-                if value ? value then
-                  if builtins.isList value.value then
-                    let
-                      count = builtins.length value.value;
-                    in
-                    if count == 0 then
-                      "()"
-                    else
-                      "(\n${
-                        lib.concatImapStringsSep "\n" (
-                          index: element:
-                          "${indent nextIndent}${toRON' nextIndent element}${lib.optionalString (index != count) ","}"
-                        ) value.value
-                      },\n${indent startIndent})"
-                  else
-                    throw "lib.cosmic.generators.toRON: tuple type must have a list of values."
-                else
-                  throw "lib.cosmic.generators.toRON: tuple type must have a value."
-              else if value.__type == "namedStruct" then
-                if value ? name then
-                  if builtins.isString value.name then
-                    if value ? value then
-                      if builtins.isAttrs value.value then
+                  "{\n${
+                    lib.concatImapStringsSep "\n" (
+                      index: entry:
+                      assert lib.assertMsg (
                         let
-                          keys = builtins.attrNames value.value;
-                          count = builtins.length keys;
+                          keys = builtins.attrNames entry;
                         in
-                        if count == 0 then
-                          "${value.name}()"
-                        else
-                          "${value.name}(\n${
-                            lib.concatImapStringsSep "\n" (
-                              index: key:
-                              "${indent nextIndent}${key}: ${toRON' nextIndent value.value.${key}}${
-                                lib.optionalString (index != count) ","
-                              }"
-                            ) keys
-                          },\n${indent startIndent})"
-                      else
-                        throw "lib.cosmic.generators.toRON: namedStruct type value must be a attribute set."
-                    else
-                      throw "lib.cosmic.generators.toRON: namedStruct type must have a value."
-                  else
-                    throw "lib.cosmic.generators.toRON: namedStruct type name must be a string."
+                        keys == [
+                          "key"
+                          "value"
+                        ]
+                      ) "lib.cosmic.generators.toRON: map type entry must have only 'key' and 'value' attributes.";
+
+                      "${indent nextIndent}${toRON' nextIndent entry.key}: ${toRON' nextIndent entry.value}${
+                        lib.optionalString (index != count) ","
+                      }"
+                    ) value.value
+                  },\n${indent startIndent}}"
+              else if value.__type == "tuple" then
+                assert lib.assertMsg (value ? value) "lib.cosmic.generators.toRON: tuple type must have a value.";
+                assert lib.assertMsg (builtins.isList value.value)
+                  "lib.cosmic.generators.toRON: tuple type value must be a list.";
+
+                let
+                  count = builtins.length value.value;
+                in
+                if count == 0 then
+                  "()"
                 else
-                  throw "lib.cosmic.generators.toRON: namedStruct type must have a name."
+                  "(\n${
+                    lib.concatImapStringsSep "\n" (
+                      index: element:
+                      "${indent nextIndent}${toRON' nextIndent element}${lib.optionalString (index != count) ","}"
+                    ) value.value
+                  },\n${indent startIndent})"
+              else if value.__type == "namedStruct" then
+                assert lib.assertMsg (
+                  value ? name
+                ) "lib.cosmic.generators.toRON: namedStruct type must have a name.";
+                assert lib.assertMsg (builtins.isString value.name)
+                  "lib.cosmic.generators.toRON: namedStruct type name must be a string.";
+                assert lib.assertMsg (
+                  value ? value
+                ) "lib.cosmic.generators.toRON: namedStruct type must have a value.";
+                assert lib.assertMsg (builtins.isAttrs value.value)
+                  "lib.cosmic.generators.toRON: namedStruct type value must be a attribute set.";
+
+                let
+                  keys = builtins.attrNames value.value;
+                  count = builtins.length keys;
+                in
+                if count == 0 then
+                  "${value.name}()"
+                else
+                  "${value.name}(\n${
+                    lib.concatImapStringsSep "\n" (
+                      index: key:
+                      "${indent nextIndent}${key}: ${toRON' nextIndent value.value.${key}}${
+                        lib.optionalString (index != count) ","
+                      }"
+                    ) keys
+                  },\n${indent startIndent})"
               else
                 throw "lib.cosmic.generators.toRON: set type ${toString value.__type} is not supported."
             else
